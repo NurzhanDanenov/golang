97d546b9b381c7056460b89f4903d37f57aef414
Nurzhan Danenov <nurdanenov@gmail.com>
2023-09-21 23:50:22 +0600

    task1

--- lecture2/task1/task1.go
+++ lecture2/task1/task1.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
-func intToRoman(num int) string {
-	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	romanNumeral := ""
+func main() {
+	num1 := 3
+	ex1 := intToRoman(num1)
+	fmt.Println(ex1)
 
-	for i := 0; i < len(values); i++ {
-		for num >= values[i] {
-			romanNumeral += symbols[i]
-			num -= values[i]
+	num2 := 58
+	ex2 := intToRoman(num2)
+	fmt.Println(ex2)
+
+	num3 := 1994
+	ex3 := intToRoman(num3)
+	fmt.Println(ex3)
+}
+
+func intToRoman(num int) string {
+	romanDigits := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	digits := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	result := ""
+
+	for i := 0; i < len(digits); i++ {
+		for num >= digits[i] {
+			result += romanDigits[i]
+			num -= digits[i]
 		}
 	}
 
-	return romanNumeral
-}
-
-func main() {
-	integer := 3549
-	roman := intToRoman(integer)
-	fmt.Printf("The Roman numeral representation of %d is %s\n", integer, roman)
+	return result
 }
