851a5d29334b8c96a47efb7d49befe010b3bb9d3
Nurzhan Danenov <nurdanenov@gmail.com>
2023-09-22 15:10:39 +0600

    task2

---
+++ lecture2/task2/task2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	list1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}
+	list2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
+	mergedList, mergedValues := mergeTwoLists(list1, list2)
+	fmt.Println(mergedList)
+	fmt.Println(mergedValues)
+
+	list1 = &ListNode{}
+	list2 = &ListNode{}
+	mergedList, mergedValues = mergeTwoLists(list1, list2)
+	fmt.Println(mergedList)
+	fmt.Println(mergedValues)
+
+	list1 = &ListNode{}
+	list2 = &ListNode{Val: 0}
+	mergedList, mergedValues = mergeTwoLists(list1, list2)
+	fmt.Println(mergedList)
+	fmt.Println(mergedValues)
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) (*ListNode, []int) {
+	mergedValues := []int{}
+	for list1 != nil {
+		mergedValues = append(mergedValues, list1.Val)
+		list1 = list1.Next
+	}
+	for list2 != nil {
+		mergedValues = append(mergedValues, list2.Val)
+		list2 = list2.Next
+	}
+
+	sort.Ints(mergedValues)
+
+	var mergedList *ListNode
+	current := mergedList
+	for _, val := range mergedValues {
+		node := &ListNode{Val: val}
+		if current == nil {
+			current = node
+			mergedList = current
+		} else {
+			current.Next = node
+			current = current.Next
+		}
+	}
+
+	return mergedList, mergedValues
+}
